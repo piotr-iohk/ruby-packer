c0452856a0c44687977d661b85cca23f5f6ba9ea
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-10-29 22:36:45 +0100

    CC=msvcc.sh

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -616,8 +616,8 @@ class Compiler
         @utils.run(compile_env,
           'sh',
           './configure',
-          'CC=./msvcc.sh',
-          'CXX=./msvcc.sh',
+          'CC=msvcc.sh',
+          'CXX=msvcc.sh',
           'LD=link',
           'CPP="cl -nologo -EP"',
           'CPPFLAGS="-DFFI_BUILDING_DLL"')
